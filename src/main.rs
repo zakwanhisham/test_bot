4dd2f61a6561cb47c9f9af9070f5d21e64b3504c
zakwan hisham <hishamzakwan@gmail.com>
2022-08-08 12:35:44 +0800

    Adding buttons example to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,66 @@
 /*
 THE FIRST HALF OF THE CODE IS FOR DICE AND COMMAND EXAMPLE
  */
-// use std::error::Error;
-// use teloxide::{prelude::*, utils::command::BotCommands};
+/* use std::error::Error;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
-// #[tokio::main]
-// async fn main() {
-//     pretty_env_logger::init();
-//     log::info!("Starting test_bot...");
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting test_bot...");
 
-//     let bot = Bot::from_env().auto_send();
+    let bot = Bot::from_env().auto_send();
 
-//     // teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-//     //     bot.send_dice(message.chat.id).await?;
-//     //     respond(())
-//     // })
-//     // .await;
+    // teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+    //     bot.send_dice(message.chat.id).await?;
+    //     respond(())
+    // })
+    // .await;
 
-//     teloxide::commands_repl(bot, answer, Command::ty()).await;
-// }
+    teloxide::commands_repl(bot, answer, Command::ty()).await;
+}
 
-// #[derive(BotCommands, Clone)]
-// #[command(rename = "lowercase", description = "These are the supported command:")]
-// enum Command {
-//     #[command(description = "display this text.")]
-//     Help,
-//     #[command(description = "handle a username.")]
-//     Username(String),
-//     #[command(description = "handle a username and an age.", parse_with = "split")]
-//     UserNameAndAge { username: String, age: u8 },
-// }
+#[derive(BotCommands, Clone)]
+#[command(rename = "lowercase", description = "These are the supported command:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UserNameAndAge { username: String, age: u8 },
+}
 
-// async fn answer(
-//     bot: AutoSend<Bot>,
-//     message: Message,
-//     command: Command,
-// ) -> Result<(), Box<dyn Error + Send + Sync>> {
-//     match command {
-//         Command::Help => {
-//             bot.send_message(message.chat.id, Command::descriptions().to_string())
-//                 .await?
-//         }
-//         Command::Username(username) => {
-//             bot.send_message(message.chat.id, format!("Your username is @{username}."))
-//                 .await?
-//         }
-//         Command::UserNameAndAge { username, age } => {
-//             bot.send_message(
-//                 message.chat.id,
-//                 format!("Your username is @{username} and your age is {age}."),
-//             )
-//             .await?
-//         }
-//     };
-//     Ok(())
-// }
+async fn answer(
+    bot: AutoSend<Bot>,
+    message: Message,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => {
+            bot.send_message(message.chat.id, Command::descriptions().to_string())
+                .await?
+        }
+        Command::Username(username) => {
+            bot.send_message(message.chat.id, format!("Your username is @{username}."))
+                .await?
+        }
+        Command::UserNameAndAge { username, age } => {
+            bot.send_message(
+                message.chat.id,
+                format!("Your username is @{username} and your age is {age}."),
+            )
+            .await?
+        }
+    };
+    Ok(())
+} */
 
 /*
 THE SECOND HALF IS FOR "CONVERSATION" EXAMPLE
 */
 
-use std::error::Error;
+/* use std::error::Error;
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
@@ -168,6 +168,147 @@ async fn receive_location(
         None => {
             bot.send_message(msg.chat.id, "Send me plain text.").await?;
         }
+    }
+    Ok(())
+} */
+
+/*
+THIS IS FOR BUTTON IN TELEGRAM
+*/
+
+use std::error::Error;
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude::*,
+    types::{
+        InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
+        InputMessageContentText,
+    },
+    utils::command::BotCommands,
+};
+
+#[derive(BotCommands)]
+#[command(
+    rename = "lowercase",
+    description = "These are the supported commands: "
+)]
+enum Command {
+    #[command(description = "Display this text")]
+    Help,
+    #[command(description = "Start")]
+    Start,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    log::info!("Starting button bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    let handler = dptree::entry()
+        .branch(Update::filter_message().endpoint(message_handler))
+        .branch(Update::filter_message().endpoint(callback_handler))
+        .branch(Update::filter_message().endpoint(inline_query_handler));
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+    Ok(())
+}
+
+// Creates a keyboard made by buttons in a big colums.
+fn make_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    let debian_versions = [
+        "Buzz", "Rex", "Bo", "Hamm", "Slink", "Potato", "Woody", "Sarge", "Etch", "Lenny",
+        "Squeeze", "Wheezy", "Jessie", "Stretch", "Buster", "Bullseye",
+    ];
+
+    for versions in debian_versions.chunks(3) {
+        let row = versions
+            .iter()
+            .map(|&version| InlineKeyboardButton::callback(version.to_owned(), version.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
+// Parse the text wrote on Telegram and check if that is a valid command
+// or not, then march the command. If the command is `/start`, it writes a
+// markup with the `InlineKeyboardMarkup`.
+async fn message_handler(
+    m: Message,
+    bot: AutoSend<Bot>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(text) = m.text() {
+        match BotCommands::parse(text, "buttons") {
+            Ok(Command::Help) => {
+                // Just send the description of all commands.
+                bot.send_message(m.chat.id, Command::descriptions().to_string())
+                    .await?;
+            }
+            Ok(Command::Start) => {
+                // Create a list of buttons and send them.
+                let keyboard = make_keyboard();
+                bot.send_message(m.chat.id, "Debian versions: ")
+                    .reply_markup(keyboard)
+                    .await?;
+            }
+            Err(_) => {
+                bot.send_message(m.chat.id, "Command not found!").await?;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn inline_query_handler(
+    q: InlineQuery,
+    bot: AutoSend<Bot>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let choose_debian_version = InlineQueryResultArticle::new(
+        "0",
+        "choose debian version",
+        InputMessageContent::Text(InputMessageContentText::new("Debian versions: ")),
+    )
+    .reply_markup(make_keyboard());
+
+    bot.answer_inline_query(q.id, vec![choose_debian_version.into()])
+        .await?;
+
+    Ok(())
+}
+
+// When it receives a callback from a button it edits the message with all
+// those buttons writing a text withe the selected Debian version.
+//
+// **IMPORTANT**: do not send privacy-sensitive data this way!!!
+// Anyone can read data stored in the callback button.
+async fn callback_handler(
+    q: CallbackQuery,
+    bot: AutoSend<Bot>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(version) = q.data {
+        let text = format!("You choose: {version}");
+
+        match q.message {
+            Some(Message { id, chat, .. }) => {
+                bot.edit_message_text(chat.id, id, text).await?;
+            }
+            None => {
+                if let Some(id) = q.inline_message_id {
+                    bot.edit_message_text_inline(id, text).await?;
+                }
+            }
+        }
+        log::info!("You choose: {}", version);
     }
     Ok(())
 }
